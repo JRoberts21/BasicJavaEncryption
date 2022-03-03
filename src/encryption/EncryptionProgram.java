d71883eafc635fa14c046275a5eae4fc2e7054c5
Jonathan Roberts <95726437+JRoberts21@users.noreply.github.com>
2022-03-02 22:02:59 -0500

    Initial commit

---
+++ src/encryption/EncryptionProgram.java
@@ -0,0 +1,125 @@
+package encryption;
+import java.util.*;
+public class EncryptionProgram {
+
+        private Scanner scanner;
+        private Random random;
+        private ArrayList<Character> list;
+        private ArrayList<Character> shuffledList;
+        private char character;
+        private String Line;
+        private char[] letters;
+
+
+        EncryptionProgram(){
+                scanner = new Scanner(System.in);
+                random = new Random();
+                list = new ArrayList();
+                shuffledList = new ArrayList();
+                character = ' ';
+
+                newKey();
+                askQuestion();
+
+        }
+        private void askQuestion(){     //determines if the user wants to encrypt or decrypt
+                while(true){
+                    System.out.println("_____________________________________________");
+                    System.out.println("What do you want to do?");
+                    System.out.println("(N)ewKey,(G)etKey,(E)ncrypt,(D)ecrypt,(Q)uit");
+                    char response = Character.toUpperCase(scanner.nextLine().charAt(0));        //checks the first letter of the input and stores it in response
+                    switch (response){
+                            case 'N':
+                                   newKey();
+                                   break;
+                            case 'G':
+                                    getKey();
+                                    break;
+                            case 'E':
+                                    encrypt();
+                                    break;
+                            case 'D':
+                                    decrypt();
+                                    break;
+                            case 'Q':
+                                    quit();
+                                    break;
+                            default:
+                                    System.out.println("Not a valid answer ");
+                    }
+                }
+        }
+        private void newKey(){      //creates a new key
+                character = ' ';
+                list.clear();           //meant to clear the list just in case its full
+                shuffledList.clear();
+
+                for (int i =32; i<127; i++){             //I'm not using any ascii characters before 32, so that I don't need to work with obscure characters
+                        list.add(Character.valueOf(character));
+                        character++;
+                }
+                shuffledList = new ArrayList(list);
+                Collections.shuffle(shuffledList);
+                System.out.println("A new key has been created ");
+
+        }
+        private void getKey(){      //returns the current key. when this method is called, the first row displays what the original letter is and the second row displays what character it will be swapped with.
+                System.out.println("key: ");
+                for (Character x: list) {
+                        System.out.print(x);
+                }
+                System.out.println();
+                for (Character x: shuffledList) {
+                        System.out.print(x);
+                }
+                System.out.println();
+        }
+        private void encrypt() {    //encrypts plain text as cipher text
+                System.out.println("Enter a message that you want to be encrypted: ");
+                String message = scanner.nextLine();
+
+                letters = message.toCharArray();
+
+                for(int i =0; i<letters.length; i++){
+
+                        for(int j =0; j<list.size(); j++){
+                                if(letters[i]==list.get(j)) {
+                                        letters[i]=shuffledList.get(j);
+                                        break;
+                                }
+
+                        }
+                }
+                System.out.println("Encrypted: ");
+                for(char x: letters) {
+                        System.out.print(x);
+                }
+                System.out.println();
+        }
+        private void decrypt(){     //takes cipher text and converts it to plain text
+                System.out.println("Enter a message that you want to be decrypted: ");
+                String message = scanner.nextLine();
+
+                letters = message.toCharArray();
+
+                for(int i =0; i<letters.length; i++){
+
+                        for(int j =0; j<shuffledList.size(); j++){
+                                if(letters[i]==shuffledList.get(j)) {
+                                        letters[i]=list.get(j);
+                                        break;
+                                }
+
+                        }
+                }
+                System.out.println("Decrypted: ");
+                for(char x: letters) {
+                        System.out.print(x);
+                }
+                System.out.println();
+        }
+        private void quit(){
+                System.out.println("Goodbye");
+                System.exit(0);
+        }
+}
---
+++ src/encryption/Main.java
@@ -0,0 +1,23 @@
+/*
+3/2/22
+This a basic java encryption program, it uses and OTP, or One-Time Pad encryption algorithm
+
+def: requires a one time pre shared key that is the same size or longer than the message being sent.
+Plaintext is paired with a random secret key.
+
+I'm using the ASCII table to generate the key
+ */
+
+
+package encryption;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        EncryptionProgram ep = new EncryptionProgram();
+
+
+
+    }
+}
